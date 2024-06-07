47becb4103d6a289092a18b3f433d034c8342a73
Anburaj S <anburaj2203@gmail.com>
2024-06-07 11:41:57 +0530

    added different diagroms

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,23 @@
 // import LinePlot from "./pages/LinePlot";
 // import LinePlotB from "./pages/LinePlotB";
 
+import BarChart from "./pages/BarChart";
 import MyChart from "./pages/MyChart";
+import PieChart from "./pages/PieChat";
+import DoughnutChart from "./pages/DoughnutChart";
 
 function App() {
   // const data = [100, 200, 300, 500, 100, 400,700];
   return (
-    <div className="flex justify-between">
+    <div className="">
       {/* <LinePlot data={data} />
       <LinePlotB data={data} /> */}
+      <div style={{width:"50%",margin:"auto",display:"flex",flexDirection:"column",gap:"100px"}}>
       <MyChart/>
+      <BarChart/>
+      <PieChart/>
+      <DoughnutChart/>
+      </div>
     </div>
   );
 }
